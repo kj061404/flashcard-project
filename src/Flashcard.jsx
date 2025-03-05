c387e680e4a86726ef529d2153eebae47765cd65
kj061404 <kobejordan2004@icloud.com>
2025-03-05 09:16:46 -0600

    Added initial layout and started on Flashcard component

--- src/Flashcard.jsx
+++ src/Flashcard.jsx
@@ -1,28 +1,5 @@
 import React, {useState} from 'react';
-
-const cards = [
-    {front: "Start!", back: "Press the next arrow to start the flashcards :)", color: "green"},
-    {front: "Easy (math): Calculate the perimeter of a rectangle with length 7 units and width 3 units", back: "Perimeter = 2(length + width) = 2(7 + 3) = 20 units", color: "green"},
-    {front: "Medium (math): Solve for x in the equation 3x-5=16 3x-5=16", back: "Add 5 to both sides: 3x=21. Then, divide by 3: x=7.", color: "yellow"},
-    {front: "Hard (math):  Solve for x: 5x+3 = 2x+18", back: "Subtract 2x and 3 from both sides to get 3x=15. Then, divide by 3: x=5", color: "orange"},
-    {front: "Impossible (math): Evaluate the integral ∫3x 2 dx from x=0 to x=2", back: "The antiderivative of 3x^2  is x^3. Evaluating from 0 to 2, we calculate [2^3]−[0^3]= 8 - 0 = 8", color: "red"},
-    {front: "Easy (English): What is the past tense of 'run'?", back: "Ran.", color: "green"},
-    {front: "Medium (English): Identify the noun in the sentence: 'The cat sat on the mat.'", back: "Nouns: cat, mat.", color: "yellow"},
-    {front: "Hard (English): Provide an example of a metaphor. Hint: Think about how we describe time in terms of money.", back: "Example: 'Time is money.'", color: "orange"},
-    {front: "Imposible (English): What is the difference between 'its' and 'it's'?", back: "'Its' is a possessive pronoun, while 'it's' is a contraction for 'it is' or 'it has.'", color: "red"},
-    {front: "Easy (Science): What is the chemical formula for water?", back: "H2O.", color: "green"},
-    {front: "Medium (Science): What is the primary gas found in Earth's atmosphere?", back: "Nitrogen (about 78%).", color: "yellow"},
-    {front: "Hard (Science): What element is represented by the symbol 'Au' on the periodic table?", back: "Gold.", color: "orange"},
-    {front: "Impossible (Science): Define mitosis.", back: "Mitosis is a process where a single cell divides to produce two identical daughter cells.", color: "red"},
-    {front: "Easy (History): When was the Declaration of Independence signed?", back: "July 4, 1776.", color: "green"},
-    {front: "Medium (History): Who was the 16th President of the United States?", back: "Abraham Lincoln, who served from 1861 to 1865.", color: "yellow"},
-    {front: "Hard (History): What event started World War I?", back: "The assassination of Archduke Franz Ferdinand in 1914.", color: "orange"},
-    {front: "Impossible (History): What does 'Iron Curtain' refer to?", back: "The division between Communist and non-Communist Europe after WWII.", color: "red"}
-]
-    
-
-
-
+import cards from './FlashcardData';
 
 
 

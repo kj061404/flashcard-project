068bd2f4e82c89bd165e45a5cd39062e5a1c1766
kj061404 <kobejordan2004@icloud.com>
2025-03-04 23:10:47 -0600

    Added initial layout and started on Flashcard component

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,17 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='root'>
+        <div className='app-container'>
+          <h2>The All-around knowledge quiz!</h2>
+          <h4>One question per difficulty: Easy, Medium, Hard, Impossible</h4>
+          <h4>Easy: Green | Medium: Yellow | Hard: Orange | Impossible: Red</h4>
+          <h4>Four subjects: Math, English, Science, History</h4>
+          <h5>Number of cards: 16</h5>
+          <Flashcard />
+          <button>⭢</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/Flashcard.jsx
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+
+const cards = [
+    {front: "Start!", back: "Press the next arrow to start the flashcards :)", color: "green"},
+    {front: "Easy (math): Calculate the perimeter of a rectangle with length 7 units and width 3 units", back: "Perimeter = 2(length + width) = 2(7 + 3) = 20 units", color: "green"},
+    {front: "Medium (math): Solve for x in the equation 3x-5=16 3x-5=16", back: "Add 5 to both sides: 3x=21. Then, divide by 3: x=7.", color: "yellow"},
+    {front: "Hard (math):  Solve for x: 5x+3 = 2x+18", back: "Subtract 2x and 3 from both sides to get 3x=15. Then, divide by 3: x=5", color: "orange"},
+    {front: "Impossible (math): Evaluate the integral ∫3x 2 dx from x=0 to x=2", back: "The antiderivative of 3x^2  is x^3. Evaluating from 0 to 2, we calculate [2^3]−[0^3]= 8 - 0 = 8", color: "red"},
+
+]
+    
+
+
+
+
+
+
+function Flashcard() {
+    
+    return (
+
+    );
+}
+
+export default Flashcard;

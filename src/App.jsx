358453ea9ce0dab727680cc71f0603d21016a7f4
kj061404 <kobejordan2004@icloud.com>
2025-03-06 10:18:46 -0600

    Completed logic and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Flashcard from './Flashcard.jsx'
 
 function App() {
   const [index, setIndex] = useState(0);
 
-  const onNextCLick = () => {
+  const onNextClick = () => {
     if (index === 15) {
       setIndex(0);
     } else {
@@ -15,20 +14,19 @@ function App() {
   }
 
   return (
-    <>
-      <div className='root'>
-        <div className='app-container'>
-          <h2>The All-around knowledge quiz!</h2>
-          <h4>One question per difficulty: Easy, Medium, Hard, Impossible</h4>
-          <h4>Easy: Green | Medium: Yellow | Hard: Orange | Impossible: Red</h4>
-          <h4>Four subjects: Math, English, Science, History</h4>
-          <h5>Number of cards: 16</h5>
-          <Flashcard cardNumber={index}/>
-          <button onClick={onNextCLick}>⭢</button>
-        </div>
+    <div className='root'>
+      <div className='app-container'>
+        <h2>The All-around knowledge quiz!</h2>
+        <h4>One question per difficulty: Easy, Medium, Hard, Impossible</h4>
+        <h4>Easy: Green | Medium: Yellow | Hard: Orange | Impossible: Red</h4>
+        <h4>Four subjects: Math, English, Science, History</h4>
+        <h5>Number of cards: 16</h5>
+        <Flashcard cardNumber={index}/>
+        <p>Press the next arrow to start the flashcards :</p>
+        <button onClick={onNextClick}>→</button>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
--- src/Flashcard.jsx
+++ src/Flashcard.jsx
@@ -1,16 +1,22 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import cards from './FlashcardData';
 
+function Flashcard({ cardNumber }) {
+    const [isFlipped, setIsFlipped] = useState(false);
 
+    const handleClick = () => {
+        setIsFlipped(!isFlipped);
+    }
 
-function Flashcard(cardNumber) {
-    
+    const cardStyle = {
+        backgroundColor: cards[cardNumber].color
+    };
+
     return (
-        <div className='flashcard' style={{backgroundColor: cards[cardNumber].color}}>
-            <div className='front'>{cards[cardNumber].front}</div>
-            <div className='back'>{cards[cardNumber].back}</div>
+        <div     className={isFlipped ? "cardBack" : "cardFront"} onClick={handleClick} style={cardStyle}>
+            <div className="front" style={cardStyle}> {isFlipped ? '' : cards[cardNumber].front} </div>
+            <div className="back" style={cardStyle}> {isFlipped ? cards[cardNumber].back : ''} </div>
         </div>
-
     );
 }
 
